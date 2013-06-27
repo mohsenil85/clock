fd3ad06642174a930d5cf8462b3d2ba8d84e6df3
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2013-06-27 15:47:41 -0700

    sort of working bin clock

--- clock.js
+++ clock.js
@@ -1,7 +1,8 @@
-var digits = ["&nbsp;eeeee <br>&nbsp;8&nbsp;&nbsp;08 <br>&nbsp;8&nbsp;&nbsp;&nbsp;8 <br>&nbsp;8&nbsp;&nbsp;&nbsp;8 <br>88&nbsp;&nbsp;&nbsp;88 <br>88eee88",
-		"&nbsp;&nbsp;&nbsp;88<br />&nbsp;&nbsp;&nbsp;&nbsp;8<br />&nbsp;&nbsp;&nbsp;&nbsp;8<br />&nbsp;&nbsp;&nbsp;&nbsp;8<br />&nbsp;&nbsp;&nbsp;8888<br />&nbsp;&nbsp;&nbsp;8888"
-];
+//var digits = ["&nbsp;eeeee <br>&nbsp;8&nbsp;&nbsp;08 <br>&nbsp;8&nbsp;&nbsp;&nbsp;8 <br>&nbsp;8&nbsp;&nbsp;&nbsp;8 <br>88&nbsp;&nbsp;&nbsp;88 <br>88eee88",
+		//"&nbsp;&nbsp;&nbsp;88<br />&nbsp;&nbsp;&nbsp;&nbsp;8<br />&nbsp;&nbsp;&nbsp;&nbsp;8<br />&nbsp;&nbsp;&nbsp;&nbsp;8<br />&nbsp;&nbsp;&nbsp;8888<br />&nbsp;&nbsp;&nbsp;8888"
+//];
 
+var digits = ["0", "1"];
 
 $(function() {
 	function displayTime() {
@@ -9,8 +10,14 @@ $(function() {
 			.getTime()
 			.toString(2);
 		var i = 0;
-		for (i = now.length; i > 0; i--) {
-			$('#' + (33 - i)).html(digits[now.charAt(i)]);
+		for (i = 0; i < now.length; i++) {
+			$('#test' + i).html('bob');
+      if(now.charAt(i) === '0'){
+			$('#test' + i).addClass('hidden');
+      } else {
+			$('#test' + i).removeClass('hidden');
+      }
+			//$('#test' + i).html(digits[now.charAt(i)]);
 		};
 		setTimeout(displayTime, 1);
 	};
