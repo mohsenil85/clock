945193937c11696ca64001288d5d01dd8efc8f95
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2013-06-27 15:48:54 -0700

    got rid of old html code in clockjs

--- clock.js
+++ clock.js
@@ -1,8 +1,4 @@
-//var digits = ["&nbsp;eeeee <br>&nbsp;8&nbsp;&nbsp;08 <br>&nbsp;8&nbsp;&nbsp;&nbsp;8 <br>&nbsp;8&nbsp;&nbsp;&nbsp;8 <br>88&nbsp;&nbsp;&nbsp;88 <br>88eee88",
-		//"&nbsp;&nbsp;&nbsp;88<br />&nbsp;&nbsp;&nbsp;&nbsp;8<br />&nbsp;&nbsp;&nbsp;&nbsp;8<br />&nbsp;&nbsp;&nbsp;&nbsp;8<br />&nbsp;&nbsp;&nbsp;8888<br />&nbsp;&nbsp;&nbsp;8888"
-//];
 
-var digits = ["0", "1"];
 
 $(function() {
 	function displayTime() {
@@ -17,7 +13,6 @@ $(function() {
       } else {
 			$('#test' + i).removeClass('hidden');
       }
-			//$('#test' + i).html(digits[now.charAt(i)]);
 		};
 		setTimeout(displayTime, 1);
 	};
