653e9412ed600d1b7ad309b4878c1b581e146df8
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2013-06-27 15:59:41 -0700

    added colored blocks

--- clock.js
+++ clock.js
@@ -4,14 +4,16 @@ $(function() {
 	function displayTime() {
 		var now = new Date()
 			.getTime()
-			.toString(2);
+			.toString(2)
+      .substring(0,33);
+      //.split("").reverse().join("");
 		var i = 0;
 		for (i = 0; i < now.length; i++) {
-			$('#test' + i).html('bob');
+			$('#test' + i).html('&nbsp;&nbsp;&nbsp;');
       if(now.charAt(i) === '0'){
-			$('#test' + i).addClass('hidden');
+			$('#test' + i).addClass('hidden').removeClass('shown');
       } else {
-			$('#test' + i).removeClass('hidden');
+			$('#test' + i).removeClass('hidden').addClass('shown');
       }
 		};
 		setTimeout(displayTime, 1);
